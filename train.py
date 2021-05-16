c35224ea4ba1633e4c4f7723aa29b53b95b9f5a3
Olle Welin <olle_welin@hotmail.com>
2021-05-16 17:52:48 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -17,9 +17,27 @@ plt.ion()   # interactive mode
 
 # Data augmentation and normalization for training
 # Just normalization for validation
+#data_transforms = {
+#    'train': transforms.Compose([
+#        transforms.RandomResizedCrop(224),
+#        transforms.RandomHorizontalFlip(),
+#        transforms.ToTensor(),
+#        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#    ]),
+#    'val': transforms.Compose([
+#        transforms.Resize(256),
+#        transforms.CenterCrop(224),
+#        transforms.ToTensor(),
+#        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#    ]),
+#}
+
+# Data augmentation and normalization for training
+# Just normalization for validation
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
