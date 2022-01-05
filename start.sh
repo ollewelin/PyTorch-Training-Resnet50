93554144ed6d721b05fbb69dcdd2cc38c5a38c66
Olle Welin <olle_welin@hotmail.com>
2022-01-05 10:58:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd ../olle_test1/home/jetson/jetson-inference/examples/my-recognition
 
-./imagenet3 --model=./model_28.onnx --input_blob=input --output_blob=output --labels=./labels.txt csi://0
+./imagenet3 --model=./model_31.onnx --input_blob=input --output_blob=output --labels=./labels.txt csi://0
