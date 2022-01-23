b3165921e4c0150133feea69bf57d6e09d3fbfa8
Olle Welin <olle_welin@hotmail.com>
2022-01-23 20:31:15 +0100

    Add files via upload

---
+++ start_rtp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../olle_test1/home/jetson/jetson-inference/examples/my-recognition
+
+./imagenet3 --model=./model_31.onnx --input_blob=input --output_blob=output --labels=./labels.txt csi://0 rtp://192.168.1.242:1234
