08200871b9e0529449198282681c684f0cd834d5
Olle Welin <olle_welin@hotmail.com>
2024-09-07 15:39:48 +0200

    Add files via upload

---
+++ restart_app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to stop and restart the application
+
+# Kill the existing process
+pkill -f imagenet4
+
+# Wait for a few seconds to ensure the process is terminated
+sleep 5
+
+# Start the application
+cd ~/jetson-inference/examples/my-recognition
+./imagenet4 --model=./model_42.onnx --input_blob=input --output_blob=output --labels=./labels.txt csi://0
